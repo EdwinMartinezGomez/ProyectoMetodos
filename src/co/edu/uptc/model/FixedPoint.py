7e26e0f207cb8f4cff6401d5be71d8b4866e5612
Edwin Martinez <edwin.martinez04@uptc.edu.co>
2024-10-12 21:33:27 -0500

    fixed input

--- src/co/edu/uptc/model/FixedPoint.py
+++ src/co/edu/uptc/model/FixedPoint.py
@@ -1,30 +1,32 @@
-import math
+import numpy as np
 
-functionMath=""
-funtionTransformed=""
+functionMath = ""
+funtionTransformed = ""
+
 def f(x):
-    return eval(functionMath, {"x": x, "math": math})
+    return eval(functionMath, {"x": x, "np": np})
 
 def g(x):
-    return eval(funtionTransformed, {"x": x, "math": math})
+    return eval(funtionTransformed, {"x": x, "np": np})
 
 def fixedPoint(functionMath, funtionTransformed, p0, tol, n, i):
-    while i<=n:
-        p=g(p0)
-        if abs(p-p0)<tol:
-            print("El punto fijo es ",p," despues de ",i," iteraciones")
+    while i <= n:
+        p = g(p0)
+        if abs(p - p0) < tol:
+            print("El punto fijo es", p, "después de", i, "iteraciones")
             break
-        i=i+1
-        p0=p
-        print("iteracion ",i-1,": ",p0)
-    if i>=n:
-        print("diverge")
+        i = i + 1
+        p0 = p
+        print("Iteración", i - 1, ": ", p0)
+    if i >= n:
+        print("El método diverge")
 
 def inputFixedPoint():
-    functionMath=input("Ingrese la funcion f(x) ")
-    funtionTransformed=input("Ingrese la funcion g(x) ")
-    p0=float(input("Ingrese el valor del punto inicial "))
-    tol=float(0.0001)
-    n=int(1000)
-    i=1
+    global functionMath, funtionTransformed 
+    functionMath = input("Ingrese la función f(x): ")
+    funtionTransformed = input("Ingrese la función g(x): ")
+    p0 = float(input("Ingrese el valor del punto inicial: "))
+    tol = float(0.0001)
+    n = int(1000)
+    i = 1
     fixedPoint(functionMath, funtionTransformed, p0, tol, n, i)
